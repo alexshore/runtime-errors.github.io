f879e961d04e553e33c4dd3719b477c943fbda27
Charles Stubbs <42813301+stubbs774@users.noreply.github.com>
2020-10-22 16:51:53 +0100

    No 2 of Coding sprint

--- Auber/core/src/com/eng/auber/AuberGame.java
+++ Auber/core/src/com/eng/auber/AuberGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.lang.Thread;
 
 public class AuberGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -14,6 +15,7 @@ public class AuberGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("runtime_errors.png");
+		img.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 	}
 
 	@Override
@@ -21,13 +23,14 @@ public class AuberGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 2 * Gdx.graphics.getWidth()/6, 2 * Gdx.graphics.getHeight()/7);
+		batch.draw(img, Gdx.graphics.getWidth()/2 - img.getWidth()/2, Gdx.graphics.getHeight()/2 - img.getHeight()/2);
+		//Centres the image
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		//img.dispose();
+		img.dispose();
 	}
 }
