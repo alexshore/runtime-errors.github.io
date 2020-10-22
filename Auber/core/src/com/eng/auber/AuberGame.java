c03bd4d5be0601cca891388538430601b759c943
Charles Stubbs <42813301+stubbs774@users.noreply.github.com>
2020-10-22 17:08:34 +0100

    Cropped Logo

--- Auber/core/src/com/eng/auber/AuberGame.java
+++ Auber/core/src/com/eng/auber/AuberGame.java
@@ -2,13 +2,17 @@ package com.eng.auber;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class AuberGame extends ApplicationAdapter {
-	SpriteBatch batch;
+	SpriteBatch batch, textBatch;
 	Texture img;
+	CharSequence str = "Runtime Errors' Production";
+	BitmapFont font;
 	
 	@Override
 	public void create () {
