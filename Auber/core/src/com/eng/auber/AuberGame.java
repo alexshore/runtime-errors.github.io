3f6af4084dcfc79896f9f0ede69a0074bb1f3315
kntrllr <kntrllr@gmail.com>
2020-11-09 14:14:54 +0000

    moved movement system

--- Auber/core/src/com/eng/auber/AuberGame.java
+++ Auber/core/src/com/eng/auber/AuberGame.java
@@ -1,58 +1,30 @@
 package com.eng.auber;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+<<<<<<< Updated upstream
 import com.badlogic.gdx.Input.Keys;
+=======
+>>>>>>> Stashed changes
 import screens.MainMenu;
 
 public class AuberGame extends Game {
 	public SpriteBatch batch;
-	Texture img;
-	float x, y;
-	int img_h, img_w;
+
 
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		this.setScreen(new MainMenu(this));//displays game screen
-		img = new Texture("game_assets/P_temp.png");
-		img_h = img.getHeight();
-		img_w = img.getWidth();
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		super.render();
 
-		if (Gdx.input.isKeyPressed(Keys.D)) {
-			x++;
-			if (x + img_w > Gdx.graphics.getWidth()) {
-				x = Gdx.graphics.getWidth() - img_w;
-			}
-		}
-		if (Gdx.input.isKeyPressed(Keys.A)) {
-			x--;
-			if (x < 0) {x = 0;}
-		}
-		if (Gdx.input.isKeyPressed(Keys.W)) {
-			y++;
-			if (y + img_h > Gdx.graphics.getHeight()) {
-				y = Gdx.graphics.getHeight() - img_h;
-			}
-		}
-		if (Gdx.input.isKeyPressed(Keys.S)) {
-			y--;
-			if (y < 0) {y = 0;}
-		}
-
-		batch.begin();
-		batch.draw(img, x, y);
-		batch.end();
 	}
 	
 	@Override
--- Auber/core/src/screens/GameScreen.java
+++ Auber/core/src/screens/GameScreen.java
@@ -5,37 +5,82 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.eng.auber.AuberGame;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import input.processors.*;
 
 public class GameScreen extends ScreenAdapter {
+    public SpriteBatch batch;
+    private int playerSpeed = 3;
     AuberGame game;
     boolean demoMode;
     TextureAtlas textureAtlas;
+    Texture player;
+    float x, y;
+    int player_h, player_w;
 
 
     public GameScreen(AuberGame game, boolean demoMode){
         this.demoMode  = demoMode;
         this.game = game;
+        this.batch = new SpriteBatch();
+        this.player = new Texture("game_assets/P_temp.png");
+        this.player_h = player.getHeight();
+        this.player_w = player.getWidth();
     }
+
     @Override
     public void show() {
         Gdx.input.setInputProcessor(
                 new GameInputProcessor()
         );
     }
+
     @Override
     public void render(float delta) {
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
+
+
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            x += playerSpeed;
+            if (x + player_w > Gdx.graphics.getWidth()) {
+                x = Gdx.graphics.getWidth() - player_w;
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            x -= playerSpeed;
+            if (x < 0) {x = 0;}
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            y += playerSpeed;
+            if (y + player_h > Gdx.graphics.getHeight()) {
+                y = Gdx.graphics.getHeight() - player_h;
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            y -= playerSpeed;
+            if (y < 0) {y = 0;}
+        }
+
+
+
+        batch.begin();
+        batch.draw(player, x, y);
+        batch.end();
+
+        batch.begin();
         if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)){
             game.setScreen(new MainMenu(game));
         }
         GameKeys.update();
-        game.batch.end();
-
+        batch.end();
     }
 
     @Override
