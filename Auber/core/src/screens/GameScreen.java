7ff1fefd757596ae3d11b7e1e9056f3079035c30
kntrllr <kntrllr@gmail.com>
2020-11-09 14:17:55 +0000

    tidied imports

--- Auber/core/src/screens/GameScreen.java
+++ Auber/core/src/screens/GameScreen.java
@@ -40,10 +40,6 @@ public class GameScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
         Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
